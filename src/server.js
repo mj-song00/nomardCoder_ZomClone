070287922b2bc5ecbecfa2fa560275215b1a543e
MinJi <Niveus128@gmail.com>
2022-06-26 15:32:49 +0900

    서버 추가

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from "express"
+import path from "path"
 
 const app = express()
+const __dirname = path.resolve()
 
-console.log('hello')
+app.set ("view engine", "pug")// 뷰 엔진 : 퍼그 설정
+app.set("views", __dirname + "/src/views") //express에 template이 어디있는지 지정
+app.use("/public", express.static(__dirname + "/src/public/")) // public url을 생성해서 유저에게 파일 공유
+app.get("/", (req, res)=> res.render("home"))// home.pug를 render해주는 route handler
 
-app.listen(3000);
+const handleListen = () => console.log(`Listeing on http://localhost:3000`)
+app.listen(3000, handleListen);
