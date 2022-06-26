f50c05f7448e8362a50d57f377fdf1892e473133
MinJi <Niveus128@gmail.com>
2022-06-26 23:43:45 +0900

    채팅기능 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const socket = new WebSocket(`ws://${window.location.host}`)// 백엔드 연결
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick")
+
+function makeMessage(type, payload){
+  const msg = {type, payload}
+  return JSON.stringify(msg)
+}
 
 function handleOpen(){
   console.log("Connetted to Server")  //connection이 open일때 사용
@@ -11,6 +17,9 @@ socket.addEventListener("open", handleOpen)
 
 socket.addEventListener("message", (message) => {
   console.log("New message: ", message.data); //메시지 받았을때 리스너
+  const li = document.createElement("li") //li생성
+  li.innerText = message.data // 채팅을 li에 넣어주기
+  messageList.append(li) // messageList저장
 }); 
 
 socket.addEventListener("close", () => {
@@ -25,8 +34,15 @@ function handleSubmit(event){
   event.preventDefault();
   const input = messageForm.querySelector("input");
   //console.log(input.value)
-  socket.send(input.value)
+  socket.send(makeMessage("new_message", input.value))
   input.value = ""
 }
 
+function handleNickSubmit(event){
+  event.preventDefault()
+  const input = nickForm.querySelector("input")
+  socket.send(makeMessage("nickname", input.value))
+}
+
 messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
